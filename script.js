3fe620b7b30616429658e1e6395dc35e33faf424
Eneje Caleb <enejecaleb@gmail.com>
2024-12-20 20:26:40 +0100

    Second site demo tutorial repository

---
+++ script.js
@@ -0,0 +1,8 @@
+let popup = document.getElementById("contact-popup");
+
+function openPopup(){
+    popup.classList.add("open-contact-popup")
+}
+function closePopup(){
+    popup.classList.remove("open-contact-popup")
+}
